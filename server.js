69a0349db0b5bdfdd7a6f5df8cd2d03bf73e7bda
Nataniel Pedersen <natanielmp@gmail.com>
2014-10-15 18:54:19 +0200

    Different methods for scraping + optimizations

--- server.js
+++ server.js
@@ -4,91 +4,17 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-function formatBook(rawString, thisPrice) {
-	var title;
-	var authors = [];
-	var isbn;
-
+function formatBook(title, authors, price, isbn) {
 	var json = { title: "", authors: [], price: "", isbn: "" }
 
-	split = rawString.split(" - ");
+	json.title = title;
+	json.authors = authors;
+	json.price = price;
+	json.isbn = isbn;
 
-	if (split[0].indexOf(" | ") == -1) {
-		/* Only run if page is book */
+	return json;
 
-		title = split[0];
-
-		if (split[1].indexOf(", ") == -1) {
-			/* If author name doesn't contain ',' */
-			authors[0] = split[1];
-		}
-
-		for (var i = 0; i < split.length; i++) {
-
-			if (split[i].indexOf(", ") > -1) {
-				/* Formats author: Firstname + Lastname */
-				var fullname = split[i].split(", ");
-				authors.push(fullname[1] + " " + fullname[0]);
-			}
-
-			if (split[i].indexOf("(") > -1) {
-				/* Formats isbn without ()*/
-				isbn = split[i].replace('(', '');
-				isbn = isbn.replace(')', '');
-			}
-		}
-
-		/* Remove duplicate authors */
-		for (var i = 1; i < authors.length; i++) {
-			if (authors[i-1] == authors[i]) {
-				authors.splice(i, 1);
-				break;
-			}
-		}
-
-		json.title = title;
-		json.authors = authors;
-		json.price = thisPrice;
-		json.isbn = isbn;
-
-		return json;
-
-	}
 }
-
-/* Scrape and parse single node */
-app.get('/node/:id', function(req, res) {
-	url = 'http://www.akademika.no/node/' + req.params.id;
-	request(url, function(error, response, html) {
-
-		if (!error) {
-			var rawString;
-			var price;
-			var jsonRes;
-			var $ = cheerio.load(html);
-
-			/* Price from .tilbud */
-			price = $('.info .price-full .sell-price .tilbud').text();
-			price = price.replace('Nettpris: ', '');
-			price = price.replace(',-', '');
-			console.log(price);
-
-			/* Title, authors and isbn from <title> */
-			rawString = $('title').text();
-			jsonRes = formatBook(rawString, price);
-
-			/* Send and log */
-			res.set('Content-Type', 'application/json');
-			res.send(jsonRes);
-			console.log(jsonRes);
-		}
-
-		fs.writeFile('book.json', JSON.stringify(jsonRes, null, 4), function(err){
-        	console.log('> File successfully written!');
-        });
-
-	});
-});
 
 /* Scrape and parse multiple nodes */
 app.get('/node/:from/:to', function(req, res) {
@@ -107,55 +33,87 @@ app.get('/node/:from/:to', function(req, res) {
 	res.send("Started parsing of " + (urls.length - 1) + " pages...");
 	console.log("> Started parsing of " + (urls.length - 1) + " pages...");
 
-	var books = { books: [] };
+	var booksFile = { books: [] };
 	var completedRequests = 0;
 	var booksFound = 0;
 	var booksTotal = urls.length - 1;
 
 	urls.forEach(function(url) {
-		request(url, function(error, response, html) {
 
-			if (!error) {
-				var rawString;
+		request(url, function(error, response, html) {
+			var $ = cheerio.load(html);
+			var rawTitle = $('title').text();
+			var splitTitle = rawTitle.split(" - ");
+
+			if (!error && response.statusCode == 200 && splitTitle[3] == "Bøker | Akademika.no") {
+
+				var title;
+				var authors = [];
 				var price;
+				var isbn;
 				var jsonRes;
-				var $ = cheerio.load(html);
+
+				/* Title from <title> */
+				title = splitTitle[0];
+
+				/* Authors from .author-full */
+				$('.author-full a').each(function(i, elem) {
+					authors[i] = $(this).text();
+				});
+
+				/* Formats authors name 'Surname, Firstname' -> 'Firstname Surname' */
+				for (var i = 0; i < authors.length; i++) {
+					if (authors[i].indexOf(", ") > -1) {
+						var fullname = authors[i].split(", ");
+						authors[i] = fullname[1] + " " + fullname[0];
+					}
+				}
+
+				/* Checks for duplicate authors */
+				for (var i = 1; i < authors.length; i++) {
+					if (authors[i-1] == authors[i]) {
+						authors.splice(i, 1);
+						break;
+					}
+				}
 
 				/* Price from .tilbud */
 				price = $('.info .price-full .sell-price .tilbud').text();
 				if (price == '') {
+					/* Hvis det er vanlig pris */
 					price = $('.info .uc-price-vanlig').text();
 					price = price.replace(',-', '');
 				} else {
+					/* Hvis det er nettpris */
 					price = price.replace('Nettpris: ', '');
 					price = price.replace(',-', '');
 				}
 
-				/* Title, authors and isbn from <title> */
-				rawString = $('title').text();
+				/* ISBN from <span itemprop="gtin13"> */
+				isbn = $('li span').attr('itemprop', 'gtin13').text();
 
 				/* Send info for json formatting */
-				jsonRes = formatBook(rawString, price);
-
-				if (jsonRes != undefined) {
-					booksFound++;
-					books.books.push(jsonRes)
-				}
-
-				process.stdout.write("> Found " + booksFound + " books on " + completedRequests + " pages, " +
-									(booksTotal - completedRequests) + " pages left. \r");
-
-			    completedRequests++;
-
-			    if (completedRequests == urls.length) {
-					fs.writeFile('books.json', JSON.stringify(books, null, 4), function(err) {
-						console.log('\n> File successfully written!');
-					});
-			    }
+				jsonRes = formatBook(title, authors, price, isbn);
+				console.log(jsonRes);
+				console.log();
+				booksFile.books.push(jsonRes)
+				booksFound++;
 
 			}
 
+			process.stdout.write("> Found " + booksFound + " books on " + completedRequests + " pages, " +
+								(booksTotal - completedRequests) + " pages left. \r");
+
+			if (completedRequests == urls.length - 1) {
+				fs.writeFile('books.json', JSON.stringify(booksFile, null, 4), function(err) {
+					console.log('\n> File successfully written!');
+				});
+			}
+
+			completedRequests++;
+
 		});
+
 	});
 
 });
